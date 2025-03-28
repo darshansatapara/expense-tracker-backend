f23140d6cf4879b9f26308eec8b8d83db14e80f1
darshansatapara <darshansatapara286@gmail.com>
2025-03-28 21:46:49 +0530

    Update server port configuration to include default fallback value in index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ dotenv.config(); // Load environment variables
     // Schedule the currency update job
     CurrencyDailyRateJob(adminDbConnection);
 
-    const PORT = process.env.PORT;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
